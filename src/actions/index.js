de3cde33654b3270d1c7389fa6e04f622aae4813
willmondal <wbmondal@aol.com>
2021-01-22 15:38:52 -0400

    ...

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,10 @@ export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
 export const FETCH_SMURFS_FAIL = "FETCH_SMURFS_FAIL";
 export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
 
+export const ADD_SMURF_START = "ADD_SMURF_START"
+export const ADD_SMURF_FAIL = "ADD_SMURF_FAIL"
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS"
+
 export const getSmurfs = () => dispatch =>{
     dispatch({type: FETCH_SMURFS_START})
     axios
@@ -17,9 +21,12 @@ export const getSmurfs = () => dispatch =>{
     })
 }
 
-// export const addSmurf = () => dispatch =>{
-
-// }
+export const addSmurf = (newSmurf) => dispatch =>{
+    dispatch({type: ADD_SMURF_START})
+    axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    
+}
 
 //Task List:
 //1. Add fetch smurfs action: 
